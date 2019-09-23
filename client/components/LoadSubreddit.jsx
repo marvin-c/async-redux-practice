6a0071d7899e0cb62de5a25d4d778b396a75f89c
Henry Peti <iamgadgetboy@gmail.com>
2019-09-24 11:15:46 +1200

    new subreddit complete

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {fetchPosts} from '../actions'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
-const LoadSubreddit = ({children, dispatch}) => (
-  <div>
-    <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-      Fetch Posts
-    </button>
-    {children}
-  </div>
-)
+class LoadSubreddit extends React.Component {
+  state = {
+    subreddit: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  render () {
+    return (
+      <div>
+        <input type='text' name='subreddit'onChange={this.handleChange} placeholder='Subreddit Name'/>
+        <button onClick={() => this.props.dispatch(fetchPosts(this.state.subreddit))}>
+       Fetch Posts
+        </button>
+        {' '}
+        {/* <button onClick={() => this.props.dispatch()}> */}
+        {/* </button> */}
+        {this.props.children}
+      </div>
+    )
+  }
+}
 
 export default connect()(LoadSubreddit)
